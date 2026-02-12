fba064ecf47bd0f01f90baaaf4742c9c51c1674e
Lucas Alexandre <58268054+lukalexandre@users.noreply.github.com>
2026-02-12 19:37:55 -0300

    Update Program.cs

--- Exercicio15/Program.cs
+++ Exercicio15/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Desafio - Simulador de compra
+
+try 
+{
+    Console.Write("Produto: ");
+    string prod = Console.ReadLine();
+    Console.Write("Preço Unitário: ");
+    decimal vUnit = decimal.Parse(Console.ReadLine());
+    Console.Write("Quantidade: ");
+    int qtd = int.Parse(Console.ReadLine());
+
+    decimal total = vUnit * qtd;
+
+    Console.WriteLine("\nResumo da compra:");
+    Console.WriteLine("Produto: " + prod);
+    Console.WriteLine("Quantidade: " + qtd);
+    Console.WriteLine("Total: R$ " + total);
+} 
+catch 
+{
+    Console.WriteLine("Erro nos dados de preço ou quantidade.");
+}

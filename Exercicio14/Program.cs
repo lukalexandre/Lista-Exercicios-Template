f16bac099d3c2c6222945a3adfe4b970b2aa7ed9
Lucas Alexandre <58268054+lukalexandre@users.noreply.github.com>
2026-02-12 19:37:10 -0300

    Update Program.cs

--- Exercicio14/Program.cs
+++ Exercicio14/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Minicalculadora com metodo
+
+Console.Write("Valor A: ");
+int valA = int.Parse(Console.ReadLine());
+Console.Write("Valor B: ");
+int valB = int.Parse(Console.ReadLine());
+Console.WriteLine("Soma: " + Somar(valA, valB));
+
+int Somar(int a, int b) => a + b;

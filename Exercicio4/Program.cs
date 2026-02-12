6d57de37932dc1b6261a9182e5946557c4a893df
Lucas Alexandre <58268054+lukalexandre@users.noreply.github.com>
2026-02-12 19:31:57 -0300

    Update Program.cs

--- Exercicio4/Program.cs
+++ Exercicio4/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Calculadora Simples
+
+Console.Write("Número 1: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Número 2: ");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine($"Soma: {a + b}");
+Console.WriteLine($"Subtração: {a - b}");
+Console.WriteLine($"Multiplicação: {a * b}");
+Console.WriteLine($"Divisão inteira: {a / b}");

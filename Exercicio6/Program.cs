0411ed17874dd3e3e0ee990b375f3796b66f49fc
lukalexandre <58268054+lukalexandre@users.noreply.github.com>
2026-02-12 16:17:17 -0300

    Update Program.cs

--- Exercicio6/Program.cs
+++ Exercicio6/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Conversao de Temperatura
+
+Console.Write("Graus Celsius: ");
+double c = double.Parse(Console.ReadLine());
+double f = (c * 9 / 5) + 32;
+Console.WriteLine($"Fahrenheit: {f}");

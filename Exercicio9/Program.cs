bb0501bea4de184dbfda3f22a1b95a966dcd3279
lukalexandre <58268054+lukalexandre@users.noreply.github.com>
2026-02-12 16:19:28 -0300

    Update Program.cs

--- Exercicio9/Program.cs
+++ Exercicio9/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Divisao segura
+
+try 
+{
+    Console.Write("Dividendo: ");
+    int d1 = int.Parse(Console.ReadLine());
+    Console.Write("Divisor: ");
+    int d2 = int.Parse(Console.ReadLine());
+    Console.WriteLine($"Resultado: {d1 / d2}");
+} 
+catch (DivideByZeroException) 
+{
+    Console.WriteLine("Erro: Não é possível dividir por zero.");
+} 
+catch (FormatException) 
+{
+    Console.WriteLine("Erro: Digite apenas números válidos.");
+}

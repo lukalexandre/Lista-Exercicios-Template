b2e9e2a987b38f6533eeb0d61872068159d808a7
Lucas Alexandre <58268054+lukalexandre@users.noreply.github.com>
2026-02-12 19:28:15 -0300

    Update Program.cs

--- Exercicio1/Program.cs
+++ Exercicio1/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.Write("Digite um número inteiro: ");
+int numero = int.Parse(Console.ReadLine());
+Console.WriteLine($"O número digitado foi: {numero}");
+Console.WriteLine($"O dobro desse número é: {numero * 2}");

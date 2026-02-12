4ccc6cffc025d9bb4c2a39c3f82f0b195cb80312
lukalexandre <58268054+lukalexandre@users.noreply.github.com>
2026-02-12 16:10:13 -0300

    Update Program.cs

--- Exercicio2/Program.cs
+++ Exercicio2/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.Write("Digite um número: ");
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine($"Antecessor: {num - 1}");
+Console.WriteLine($"Sucessor: {num + 1}");

9f0e97f2c8589e8483ab77a03c0789c94d6445c1
lukalexandre <58268054+lukalexandre@users.noreply.github.com>
2026-02-12 16:11:13 -0300

    Update Program.cs

--- Exercicio3/Program.cs
+++ Exercicio3/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.Write("Digite o primeiro número: ");
+int n1 = int.Parse(Console.ReadLine());
+Console.Write("Digite o segundo número: ");
+int n2 = int.Parse(Console.ReadLine());
+Console.WriteLine($"A soma de {n1} e {n2} é {n1 + n2}");
